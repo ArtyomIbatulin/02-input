c408341144059bd1013eff97506da19fb4b60c02
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-10-31 04:16:34 +0300

    feat: add DOM generation with styles

--- main.js
+++ main.js
@@ -1,5 +1,122 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-    <h1>Hello Vite!</h1>
-`;
+const app = document.querySelector("#app");
+
+const header = document.createElement("header");
+header.className = "header";
+
+const headerTitle = document.createElement("h4");
+headerTitle.className = "header__title";
+headerTitle.textContent = "DS";
+
+header.appendChild(headerTitle);
+
+const main = document.createElement("main");
+main.className = "main";
+
+const mainTitle = document.createElement("h1");
+mainTitle.className = "main__title";
+mainTitle.textContent = "Кастомный input";
+
+const mainDescription = document.createElement("h4");
+mainDescription.className = "main__description";
+mainDescription.textContent = "Это кастомный input";
+
+const form = document.createElement("form");
+form.action = "";
+form.className = "main__form";
+
+const fileInput = document.createElement("input");
+fileInput.type = "file";
+fileInput.multiple = true;
+fileInput.hidden = true;
+fileInput.id = "upload";
+fileInput.accept = ".jpg, .jpeg, .png";
+fileInput.className = "main__file-input";
+
+const labelInput = document.createElement("label");
+labelInput.htmlFor = "upload";
+labelInput.className = "main__label-input";
+
+const spanLabel = document.createElement("span");
+spanLabel.innerHTML = "	&#8853;";
+spanLabel.className = "label-input__span";
+
+const pLabel = document.createElement("p");
+pLabel.textContent = "Click to upload";
+pLabel.className = "label-input__p";
+
+const submitButton = document.createElement("button");
+submitButton.type = "submit";
+submitButton.className = "main__submit-button";
+submitButton.textContent = "Отправить";
+
+const fileDescription = document.createElement("h4");
+fileDescription.className = "main__file-description";
+fileDescription.textContent =
+  "Вы можете загрузить до 5 файлов JPG, JPEG, PNG, размер одного — до 10 МБ";
+
+const fileWrapper = document.createElement("div");
+fileWrapper.className = "main__fileWrapper";
+
+const uploadedFilesDescription = document.createElement("h3");
+uploadedFilesDescription.className = "fileWrapper__uploaded-files-description";
+uploadedFilesDescription.textContent = "Uploaded files";
+
+const showFileBox = document.createElement("div");
+showFileBox.className = "fileWrapper__show-file-box";
+
+const leftSide = document.createElement("div");
+leftSide.className = "show-file-box__leftSide";
+
+const spanLeftSide = document.createElement("span");
+spanLeftSide.className = "left-side__span";
+spanLeftSide.innerHTML = "Расширение";
+
+const nameFile = document.createElement("h3");
+nameFile.className = "left-side__name-file";
+nameFile.textContent = "Название файла";
+
+const rightSide = document.createElement("div");
+rightSide.className = "show-file-box__rightSide";
+
+const spanRightSide = document.createElement("span");
+spanRightSide.className = "right-side__span";
+spanRightSide.innerHTML = "&#215;";
+
+labelInput.appendChild(spanLabel);
+labelInput.appendChild(pLabel);
+
+form.appendChild(fileInput);
+form.appendChild(labelInput);
+form.appendChild(submitButton);
+
+leftSide.appendChild(spanLeftSide);
+leftSide.appendChild(nameFile);
+
+rightSide.appendChild(spanRightSide);
+
+showFileBox.appendChild(leftSide);
+showFileBox.appendChild(rightSide);
+
+fileWrapper.appendChild(uploadedFilesDescription);
+fileWrapper.appendChild(showFileBox);
+
+main.appendChild(mainTitle);
+main.appendChild(mainDescription);
+main.appendChild(form);
+main.appendChild(fileDescription);
+main.appendChild(fileWrapper);
+
+const footer = document.createElement("footer");
+footer.className = "footer";
+
+const footerTitle = document.createElement("h4");
+footerTitle.className = "footer__title";
+footerTitle.textContent = "DS ноябрь 2024";
+
+footer.appendChild(footerTitle);
+
+app.appendChild(header);
+app.appendChild(main);
+app.appendChild(footer);
